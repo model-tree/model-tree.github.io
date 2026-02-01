cc06ef22a87c2fe098c9934369fcf8ea47b19b33
Rares Stan <rares1928@gmail.com>
2026-02-01 19:50:58 +0100

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,9 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
-const basename = import.meta.env.PROD ? '/model-tree.github.io' : ''
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/model-tree.github.io/' : '/',
+  base: '/',
   build: {
     outDir: 'dist',
   },
